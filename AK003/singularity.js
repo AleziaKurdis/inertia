42adb690639346c17e51a36add471127f32a9b19
Alezia Kurdis <60075796+AleziaKurdis@users.noreply.github.com>
2023-01-21 14:53:02 -0500

    Add files via upload

--- AK003/singularity.js
+++ AK003/singularity.js
@@ -186,7 +186,7 @@
     function getCurrentSunPosition() {
         var axisOne = Math.sin(GetCurrentCycleValue((2* Math.PI), HYTRION_DAY_DURATION/48));
         var axisTwo = Math.cos(GetCurrentCycleValue(Math.PI, HYTRION_DAY_DURATION/48));
-        return {"x": 0, "y": (axisOne * 500), "z": (axisTwo * 1000)};
+        return {"x": (axisTwo * 1000), "y": (axisOne * 500), "z": 0};
     }
 
     function updateStar() {
