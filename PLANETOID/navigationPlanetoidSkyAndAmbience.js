fde608f8d61ecd773edae734ede419b100df4f14
Alezia Kurdis <60075796+AleziaKurdis@users.noreply.github.com>
2023-03-19 01:13:54 -0400

    Add files via upload

--- PLANETOID/navigationPlanetoidSkyAndAmbience.js
+++ PLANETOID/navigationPlanetoidSkyAndAmbience.js
@@ -243,7 +243,7 @@
                 "useOriginalPivot": true,
                 "renderWithZones": universeRenderWithZones,
                 "dimensions": {"x": 0.2868, "y": 3.5952, "z": 0.3296}
-            }, "domain"); //############################################################### Reset to local
+            }, "local");
             
             lightID =  Entities.addEntity({
                 "type": "Light",
