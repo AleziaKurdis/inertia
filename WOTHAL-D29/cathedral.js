259f661ad2978f29eee45e6b4938eac9c640b49d
Alezia Kurdis <60075796+AleziaKurdis@users.noreply.github.com>
2023-04-17 22:35:26 -0400

    Add files via upload

--- WOTHAL-D29/cathedral.js
+++ WOTHAL-D29/cathedral.js
@@ -129,7 +129,7 @@
     }
 
     function updatePlanet() {
-        var DISTANCE_FROM_CENTER =  {"x": 0, "y": 0, "z": 5};
+        var DISTANCE_FROM_CENTER =  {"x": 0, "y": 0, "z": 4};
         var angle = GetCurrentCycleValue(360, WEEK_DURATION);
         var planetLocalPosition = Vec3.sum({"x": 0.0, "y": 180, "z": 0.0}, Vec3.multiplyQbyV( Quat.fromVec3Degrees({"x": 0, "y": angle, "z": 0}), DISTANCE_FROM_CENTER ));
         if (planetId === Uuid.NULL) {
@@ -142,7 +142,7 @@
                     "localPosition": planetLocalPosition,
                     "name": "Coloweek",
                     "color": {"red": 100, "green": 100, "blue": 100 },
-                    "dimensions": {"x": 1, "y": 1, "z": 1},
+                    "dimensions": {"x": 0.618, "y": 0.618, "z": 0.618},
                     "angularDamping": 0,
                     "angularVelocity": {"x": 0, "y": (2 * Math.PI)/72.5, "z": 0}
                 }, "local");
@@ -152,10 +152,10 @@
                     "shape": "Sphere",
                     "parentID": planetId,
                     "renderWithZones": renderWithZones,
-                    "localPosition": {"x": 0, "y": 0, "z": 1},
+                    "localPosition": {"x": 0, "y": 0, "z": 1.5},
                     "name": "Moonut",
                     "color": {"red": 100, "green": 100, "blue": 100 },
-                    "dimensions": {"x": 0.2, "y": 0.2, "z": 0.2}
+                    "dimensions": {"x": 0.1, "y": 0.1, "z": 0.1}
                 }, "local");
         } else {
             //update
