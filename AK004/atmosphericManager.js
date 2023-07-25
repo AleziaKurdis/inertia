c4026bcb835319c306d085327ae0478fe6e06726
Alezia Kurdis <60075796+AleziaKurdis@users.noreply.github.com>
2023-07-25 19:35:11 -0400

    Add files via upload

--- AK004/atmosphericManager.js
+++ AK004/atmosphericManager.js
@@ -104,7 +104,7 @@
         intensity = 0.5 + (Math.cos((hour/24) * (2 * Math.PI)) * 1.5);
         
         //var zoneRotation = Quat.fromVec3Radians({"x": Math.sin((GetCurrentCycleValue(today.getTime(), 2 * Math.PI, MONTH_DURATION)) * (2 * Math.PI)), "y": 0.0, "z": GetCurrentCycleValue(today.getTime(), Math.PI, D29_HOUR_DURATION/6)});
-        var zoneRotation = Quat.fromVec3Radians({"x": 0.0, "y": 0.0, "z": GetCurrentCycleValue(today.getTime(), Math.PI, D29_HOUR_DURATION/6)});
+        var zoneRotation = Quat.fromVec3Radians({"x": 0.0, "y": 0.0, "z": - GetCurrentCycleValue(today.getTime(), Math.PI, D29_HOUR_DURATION/6)});
 
 
         var anglVelo = 0;//(Math.PI/(D29_HOUR_DURATION/6)) * 2 * MAX_COUNT;
