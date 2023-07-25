6101bd8b89f21bba7f7f782bac9d3fcbf0caa8cd
Alezia Kurdis <60075796+AleziaKurdis@users.noreply.github.com>
2023-07-25 19:43:58 -0400

    Add files via upload

--- AK004/atmosphericManager.js
+++ AK004/atmosphericManager.js
@@ -104,14 +104,14 @@
         intensity = 0.5 + (Math.cos((hour/24) * (2 * Math.PI)) * 1.5);
         
         //var zoneRotation = Quat.fromVec3Radians({"x": Math.sin((GetCurrentCycleValue(today.getTime(), 2 * Math.PI, MONTH_DURATION)) * (2 * Math.PI)), "y": 0.0, "z": GetCurrentCycleValue(today.getTime(), Math.PI, D29_HOUR_DURATION/6)});
-        var zoneRotation = Quat.fromVec3Radians({"x": 0.0, "y": 0.0, "z": GetCurrentCycleValue(today.getTime(), Math.PI, D29_HOUR_DURATION/6) - Math.PI});
+        var zoneRotation = Quat.fromVec3Radians({"x": 0.0, "y": 0.0, "z": GetCurrentCycleValue(today.getTime(), Math.PI, D29_HOUR_DURATION/6)});
 
 
         var anglVelo = 0;//(Math.PI/(D29_HOUR_DURATION/6)) * 2 * MAX_COUNT;
         var ambientIntensity = intensity/10;
         var currentsky = "https://aleziakurdis.github.io/inertia/AK004/images/sky.jpg";
         
-        print("version: 100");
+        print("version: 101");
         print("zoneRotation: " + JSON.stringify({"x": Math.sin((GetCurrentCycleValue(today.getTime(), 2 * Math.PI, MONTH_DURATION)) * (2 * Math.PI)), "y": 0.0, "z": GetCurrentCycleValue(today.getTime(), Math.PI, D29_HOUR_DURATION/6)}));
         print("anglVelo: " + anglVelo);
         
