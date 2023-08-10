0cd81e280de5d0140d01ea45739ea20cbf136b22
Alezia Kurdis <60075796+AleziaKurdis@users.noreply.github.com>
2023-08-10 00:29:16 -0400

    Add files via upload

--- AK004/atmosphericManager.js
+++ AK004/atmosphericManager.js
@@ -115,7 +115,7 @@
         }
         var zoneRotation = Quat.fromVec3Radians({"x": spin * (Math.PI/8) * (Math.sin((GetCurrentCycleValue(today.getTime(), 2 * Math.PI, MONTH_DURATION)) * (2 * Math.PI))), "y": 0.0, "z": ((Math.PI/3) * Math.sin(cycle))});
 
-        var anglVelo = spin * ((2* Math.PI/3)/(D29_HOUR_DURATION/HOUR_DIV));
+        var anglVelo = spin * (updateTimerInterval * MAX_COUNT) * ((2 * Math.PI/3)/(D29_HOUR_DURATION/HOUR_DIV));
         var ambientIntensity = intensity/10;
         var currentsky = "https://aleziakurdis.github.io/inertia/AK004/images/sky.jpg";
         
