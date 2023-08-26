bedbffd36ea6d0ebdfe34fcb599782f273bc830f
Alezia Kurdis <60075796+AleziaKurdis@users.noreply.github.com>
2023-08-25 21:59:14 -0400

    Add files via upload

--- PLANETOID/planetarGravityWithHollowGravity.js
+++ PLANETOID/planetarGravityWithHollowGravity.js
@@ -51,7 +51,8 @@
         var direction = Vec3.subtract(MyAvatar.position, planet.position);
         var localUp = Quat.getUp(MyAvatar.orientation);
         if (Vec3.distance(planet.position, MyAvatar.position) <= HOLLOW_GRAVITY_INVERSION_RADIUS) {
-            localUp = Quat.inverse(localUp);
+            //localUp = Quat.inverse(localUp);
+            localUp = Quat.getUp(Quat.inverse(MyAvatar.orientation));
         }
         MyAvatar.orientation = Quat.normalize(Quat.multiply(Quat.rotationBetween(localUp, direction), MyAvatar.orientation)); 
         if (hmdMode === false) {
