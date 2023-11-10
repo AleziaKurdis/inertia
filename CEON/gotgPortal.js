fb3082ac46ace6f1d9d0551da300399d30ca0b67
Alezia Kurdis <60075796+AleziaKurdis@users.noreply.github.com>
2023-11-09 22:31:57 -0500

    Add files via upload

--- CEON/gotgPortal.js
+++ CEON/gotgPortal.js
@@ -64,7 +64,7 @@
         for (index = 0; index < items.length; index++) {
             tmp = items[index].split("=");
             if (tmp[0] === parameterName) {
-                result = decodeURIComponent(tmp[1])
+                result = decodeURIComponent(tmp[1]);
             };
         }
         return result;
