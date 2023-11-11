43203fb7d788630bc84e2d690f252b66d5f64dcb
Alezia Kurdis <60075796+AleziaKurdis@users.noreply.github.com>
2023-11-11 13:23:57 -0500

    Add files via upload

--- CEON/gotgPortal.js
+++ CEON/gotgPortal.js
@@ -14,16 +14,16 @@
     var ROOT = Script.resolvePath('').split(jsMainFileName)[0];
     
     //################################# PORTAL LIST #############################################################
-    
+   
     var portals;
-    if (location.protocol === "hifi") {
+ /*    if (location.protocol === "hifi") {
         portals = Script.require(ROOT + "portals_domain.json");
     } else {
         serverlessReachDestination();
         portals = Script.require(ROOT + "portals_serverless.json");
     }
     //print("PORTALS: " + JSON.stringify(portals));
-    
+*/    
     for (i = 0; i < portals.length; i++) {
         portals[i].id = Uuid.NULL;
         portals[i].zoneID = Uuid.NULL;
