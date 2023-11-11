c02335efc760423ea9deffb5f0493c139f53aa97
Alezia Kurdis <60075796+AleziaKurdis@users.noreply.github.com>
2023-11-10 21:52:55 -0500

    Add files via upload

--- CEON/gotgPortal.js
+++ CEON/gotgPortal.js
@@ -61,10 +61,10 @@
         var result = null;
         var tmp = [];
         var thisLocation = location.href;
-        print("GOTG thisLocation: " + thisLocation);
-/*        var theParam = thisLocation.split("?");
+        var theParam = thisLocation.split("?");
         var items = theParam[1].split("&");
-        for (index = 0; index < items.length; index++) {
+        print("GOTG thisLocation: " + JSON.stringify(items));
+        /*for (index = 0; index < items.length; index++) {
             tmp = items[index].split("=");
             if (tmp[0] === parameterName) {
                 //result = decodeURIComponent(tmp[1]);
@@ -75,7 +75,6 @@
         return result;
     }
     
-    print("TEST: " + findGetParameter("test"));
     //#####################################################
     
     this.preload = function(entityID) {
