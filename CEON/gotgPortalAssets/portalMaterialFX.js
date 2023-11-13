b72d728fdb22d7f8e42ee5ddf43768ca340f8ceb
Alezia Kurdis <60075796+AleziaKurdis@users.noreply.github.com>
2023-11-12 22:28:49 -0500

    Add files via upload

--- CEON/gotgPortalAssets/portalMaterialFX.js
+++ CEON/gotgPortalAssets/portalMaterialFX.js
@@ -18,7 +18,7 @@
     var bloomFactor = 2;
     
 
-    var UPDATE_TIMER_INTERVAL = 200; // 0.2 sec 
+    var UPDATE_TIMER_INTERVAL = 400; // 0.4 sec 
     var processTimer = 0;
     var MIN_DISTANCE_TO_STAY_VISIBLE = 200; //in meters
     
