18990b000ed6c74e768e83004f984d7134ada565
Alezia Kurdis <60075796+AleziaKurdis@users.noreply.github.com>
2023-11-17 22:48:41 -0500

    Add files via upload

--- CEON/theEdgeBuilder.js
+++ CEON/theEdgeBuilder.js
@@ -24,6 +24,7 @@
         
         //Material LIGHT
         var id = addLightMaterial(entityID, renderWithZones);
+        print("ID: " + id);
         entitiesToBeDeleted.push(id);
         
         //Material LIGHT2
