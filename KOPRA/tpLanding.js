b61cecba5f781b48106ad3a431cea0ec2a009e44
Alezia Kurdis <60075796+AleziaKurdis@users.noreply.github.com>
2024-04-04 22:28:18 -0400

    Add files via upload

---
+++ KOPRA/tpLanding.js
@@ -0,0 +1,180 @@
+//
+//  tpLanding.js
+//
+//  Created by Alezia Kurdis, April 4th, 2024.
+//  Copyright 2024, Overte e.V.
+//
+//  Generate color tp, sign, particle light and sound on tp Landing modules.
+//
+//  Distributed under the Apache License, Version 2.0.
+//  See the accompanying file LICENSE or http://www.apache.org/licenses/LICENSE-2.0.html
+//
+(function(){
+    const ROOT = Script.resolvePath('').split("tpLanding.js")[0];
+    let tpLandingItems = [];
+
+    this.preload = function(entityID) {
+        let color, colorLight, name, model, userData, id, alignment;
+        let properties = Entities.getEntityProperties(entityID, ["position", "renderWithZones", "userData"]);
+        if (properties.userData === "") {
+            userData = {
+                "hue": 0,
+                "landingBay": "0A",
+                "tpUrl": "",
+                "signUrl": ""
+            };
+            Entities.editEntity(entityID, {"userData": JSON.stringify(userData)});
+        } else {
+            userData = JSON.parse(properties.userData);
+        }
+        colorLight = hslToRgb((userData.hue/360), 1, 0.65);
+        color = hslToRgb((userData.hue/360), 1, 0.5);
+        
+        let signImageUrl = userData.signUrl;
+        if (userData.tpUrl === "back")
+        switch(userData.tpUrl) {
+            case "":
+                signImageUrl = ROOT + "images/undefinedDestination.jpg";
+                break;
+            case "back":
+                signImageUrl = ROOT + "images/back.jpg";
+                break;
+            default:
+                if (userData.signUrl === "") {
+                    signImageUrl = ROOT + "images/undefinedDestination.jpg";
+                } else {
+                    signImageUrl = userData.signUrl;
+                }
+        }
+    
+        //material Glow
+        let bloomFactor = 2.7;
+        let materialContent = {
+            "materialVersion": 1,
+            "materials": [
+                {
+                    "name": "plasma",
+                    "albedo": [1, 1, 1],
+                    "metallic": 1,
+                    "roughness": 1,
+                    "emissive": [(colorLight[0]/255) * bloomFactor, (colorLight[1]/255) * bloomFactor, (colorLight[2]/255) * bloomFactor],
+                    "cullFaceMode": "CULL_NONE",
+                    "model": "hifi_pbr"
+                }
+            ]
+        };
+                
+        id = Entities.addEntity({
+            "type": "Material",
+            "parentID": entityID,
+            "renderWithZones": properties.renderWithZones,
+            "localPosition": {"x": 0.0, "y": 0.0, "z": 0.0},
+            "name": "plasma-material",
+            "materialURL": "materialData",
+            "priority": 3,
+            "parentMaterialName": "mat::LIGHT",
+            "materialData": JSON.stringify(materialContent)
+        }, "local");
+        tpLandingItems.push(id);
+
+        //material Sign
+        materialContent = {
+            "materialVersion": 1,
+            "materials": [
+                {
+                    "name": "sign",
+                    "albedo": [1, 1, 1],
+                    "albedoMap": signImageUrl,
+                    "metallic": 0.01,
+                    "roughness": 0.1,
+                    "emissiveMap": signImageUrl,
+                    "cullFaceMode": "CULL_NONE",
+                    "model": "hifi_pbr"
+                }
+            ]
+        };
+                
+        id = Entities.addEntity({
+            "type": "Material",
+            "parentID": entityID,
+            "renderWithZones": properties.renderWithZones,
+            "localPosition": {"x": 0.0, "y": 0.0, "z": 0.0},
+            "name": "sign-material",
+            "materialURL": "materialData",
+            "priority": 3,
+            "parentMaterialName": "mat::SIGN",
+            "materialData": JSON.stringify(materialContent)
+        }, "local");
+        tpLandingItems.push(id);
+        
+        //Landing Bay Text
+        id = Entities.addEntity({
+            "type": "Text",
+            "parentID": entityID,
+            "renderWithZones": properties.renderWithZones,
+            "localPosition": {"x": -2.5, "y": 4.2, "z": -6},
+            "localRotation": Quat.fromVec3Degrees( {"x": -90.0, "y": 180.0, "z": 0.0} ),
+            "name": "landingBay-" + userData.landingBay,
+            "dimensions": {"x": 4, "y": 3.2, "z": 0.01},
+            "text": userData.landingBay,
+            "lineHeight": 3,
+            "textColor": {"red": color[0], "green": color[1], "blue": color[2]},
+            "textAlpha": 0.55,
+            "backgroundAlpha": 0,
+            "unlit": true,
+            "alignment": "center"
+        }, "local");
+        tpLandingItems.push(id);
+
+        //tp intereactive box
+        
+        //observatory trigger
+        
+        //tp light
+        
+        //tpfx & sound
+        
+    };
+
+    this.unload = function(entityID) {
+        let i;
+        for (i=0; i < tpLandingItems.length; i++) {
+            Entities.deleteEntity(tpLandingItems[i]);
+        }
+        tpLandingItems = [];
+    };    
+
+    /*
+     * Converts an HSL color value to RGB. Conversion formula
+     * adapted from http://en.wikipedia.org/wiki/HSL_color_space.
+     * Assumes h, s, and l are contained in the set [0, 1] and
+     * returns r, g, and b in the set [0, 255].
+     *
+     * @param   {number}  h       The hue
+     * @param   {number}  s       The saturation
+     * @param   {number}  l       The lightness
+     * @return  {Array}           The RGB representation
+     */
+    function hslToRgb(h, s, l){
+        let r, g, b;
+        if(s == 0){
+            r = g = b = l; // achromatic
+        }else{
+            let hue2rgb = function hue2rgb(p, q, t){
+                if(t < 0) t += 1;
+                if(t > 1) t -= 1;
+                if(t < 1/6) return p + (q - p) * 6 * t;
+                if(t < 1/2) return q;
+                if(t < 2/3) return p + (q - p) * (2/3 - t) * 6;
+                return p;
+            }
+            let q = l < 0.5 ? l * (1 + s) : l + s - l * s;
+            let p = 2 * l - q;
+            r = hue2rgb(p, q, h + 1/3);
+            g = hue2rgb(p, q, h);
+            b = hue2rgb(p, q, h - 1/3);
+        }
+        return [Math.round(r * 255), Math.round(g * 255), Math.round(b * 255)];
+    }
+
+})
