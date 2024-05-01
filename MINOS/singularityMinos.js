38f6e873f9bc9923d3c47c182e224aab93750b7b
Alezia Kurdis <60075796+AleziaKurdis@users.noreply.github.com>
2024-04-30 22:03:10 -0400

    Add files via upload

--- MINOS/singularityMinos.js
+++ MINOS/singularityMinos.js
@@ -76,7 +76,7 @@
                 "renderWithZones": renderWithZones,
                 "damping": 0,
                 "angularDamping": 0,
-                "angularVelocity": {"x": 0.0, "y": 0.03, "z": 0.2}
+                "angularVelocity": {"x": 0.2, "y": 0.03, "z": 0.0}
         }, "local");
 
         var compagnonStarId = Entities.addEntity({
