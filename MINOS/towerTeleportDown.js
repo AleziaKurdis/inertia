77e74e47a887e9baed50a051fe30efbf1e8ab4ee
Alezia Kurdis <60075796+AleziaKurdis@users.noreply.github.com>
2024-05-01 21:38:31 -0400

    Add files via upload

---
+++ MINOS/towerTeleportDown.js
@@ -0,0 +1,29 @@
+//
+//  towerTeleportDown.js
+//
+//  Created by Alezia Kurdis, May 1st, 2024.
+//  Copyright 2024, Overte e.V.
+//
+//  tower tp down.
+//
+//  Distributed under the Apache License, Version 2.0.
+//  See the accompanying file LICENSE or http://www.apache.org/licenses/LICENSE-2.0.html
+//
+(function(){
+    var ROOT = Script.resolvePath('').split("towerTeleportDown.js")[0];
+    var justTP = true;
+    
+    this.preload = function(entityID) {
+        justTP = true;
+    }
+
+    this.enterEntity = function(entityID) {
+        //if (!justTP) {
+            MyAvatar.position = Vec3.sum(MyAvatar.position, Vec3.multiplyQbyV((MyAvatar.orientation, {"x": 0, "y": -14.5, "z": 0}));
+        //}
+    }; 
+    
+    this.leaveEntity = function(entityID) {
+        justTP = false;
+    };
+})
---
+++ MINOS/towerTeleportUp.js
@@ -0,0 +1,29 @@
+//
+//  towerTeleportUp.js
+//
+//  Created by Alezia Kurdis, May 1st, 2024.
+//  Copyright 2024, Overte e.V.
+//
+//  tower tp down.
+//
+//  Distributed under the Apache License, Version 2.0.
+//  See the accompanying file LICENSE or http://www.apache.org/licenses/LICENSE-2.0.html
+//
+(function(){
+    var ROOT = Script.resolvePath('').split("towerTeleportUp.js")[0];
+    var justTP = false;
+    
+    this.preload = function(entityID) {
+        justTP = false;
+    }
+
+    this.enterEntity = function(entityID) {
+        //if (!justTP) {
+            MyAvatar.position = Vec3.sum(MyAvatar.position, Vec3.multiplyQbyV((MyAvatar.orientation, {"x": 0, "y": 14.5, "z": 0}));
+        //}
+    }; 
+    
+    this.leaveEntity = function(entityID) {
+        justTP = false;
+    };
+})
