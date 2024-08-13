82e8a3761af19a2eb56e3cdbff84f6f15b17fb6b
Alezia Kurdis <60075796+AleziaKurdis@users.noreply.github.com>
2024-08-12 22:02:59 -0400

    Add files via upload

--- FACING-WORLDS/gravityClock.js
+++ FACING-WORLDS/gravityClock.js
@@ -99,7 +99,7 @@
                 "parentID": thisID,
                 "localPosition": {
                     "x": 0, 
-                    "y": (thisDimensions.y * 0.5) * 0.3, 
+                    "y": 0, //(thisDimensions.y * 0.5) * 0.3, 
                     "z": 0.02
                 },
                 "grab": {
