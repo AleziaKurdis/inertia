1826bc044dd8b50cdada6985d159e767e7667bea
Alezia Kurdis <60075796+AleziaKurdis@users.noreply.github.com>
2024-08-17 16:32:30 -0400

    Add files via upload

--- FACING-WORLDS/tower.js
+++ FACING-WORLDS/tower.js
@@ -602,7 +602,7 @@
             "ignoreForCollisions": true,
             "shape": "Cube",
             "script": ROOT + "localTeleporter.js",
-            "description": '{"team": "' + team + '", "teamColor": ' + JSON.stringify(lightColor) + ', "localPosition": { "x": 1.7554, "y": 53.0559, "z": -3.3364}, "localRotation": ' + JSON.stringify(Quat.fromVec3Degrees( { "x": 0, "y": 270, "z": 0} ))+ '}'
+            "description": '{"team": "' + team + '", "teamColor": ' + JSON.stringify(lightColor) + ', "localPosition": { "x": 1.7554, "y": 53.0559, "z": -8.3364}, "localRotation": ' + JSON.stringify(Quat.fromVec3Degrees( { "x": 0, "y": 270, "z": 0} ))+ '}'
         }, "local");
         entitiesToDelete.push(id);
 
