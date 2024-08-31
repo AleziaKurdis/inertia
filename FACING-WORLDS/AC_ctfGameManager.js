6a80225539ea8b3e7b5939c7ef4c567c35cde4a8
Alezia Kurdis <60075796+AleziaKurdis@users.noreply.github.com>
2024-08-31 13:53:22 -0400

    Add files via upload

--- FACING-WORLDS/AC_ctfGameManager.js
+++ FACING-WORLDS/AC_ctfGameManager.js
@@ -203,6 +203,8 @@ function myTimer(deltaTime) {
         var currentRedFlagPosition = Entities.getEntityProperties(flagRedID,["position"]).position;
         //EntityViewer.queryOctree();
         var currentBlueFlagPosition = Entities.getEntityProperties(flagBlueID,["position"]).position;
+        print("CTF: currentRedFlagPosition:" + JSON.stringify(currentRedFlagPosition)); //##################################################################
+        print("CTF: currentBlueFlagPosition" + JSON.stringify(currentBlueFlagPosition)); //##################################################################
         if (Vec3.distance(currentRedFlagPosition, FLAG_HOME_RED) > 0.3) {
             //flag possibly taken
             if (Vec3.distance(currentRedFlagPosition, FLAG_TRAP_BLUE_SIDE) < 0.3 && flagBlueStatus === "HOME") {
@@ -242,7 +244,6 @@ function myTimer(deltaTime) {
                     }
                 } else if (holder === "RED") {
                     //Returning the flag
-                    print("CTF: RED_FLAG_RETURNED");
                     EntityViewer.queryOctree();
                     Entities.editEntity(flagRedID, {"position": FLAG_HOME_RED, "rotation": Quat.fromVec3Degrees( {"x": 0, "y": 90, "z": 0 } ), "velocity": {"x": 0, "y": 0, "z": 0 }});
                     EntityViewer.queryOctree();
@@ -295,7 +296,6 @@ function myTimer(deltaTime) {
                     }
                 } else if (holder === "BLUE") {
                     //Returning the flag
-                    print("CTF: BLUE_FLAG_RETURNED");
                     EntityViewer.queryOctree();
                     Entities.editEntity(flagBlueID, {"position": FLAG_HOME_BLUE, "rotation": Quat.fromVec3Degrees( {"x": 0, "y": 90, "z": 0 } ), "velocity": {"x": 0, "y": 0, "z": 0 }});
                     EntityViewer.queryOctree();
