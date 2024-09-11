979995bbce533264fbed9c49f139b41522078c0a
Alezia Kurdis <60075796+AleziaKurdis@users.noreply.github.com>
2024-09-10 22:01:29 -0400

    Add files via upload

--- FACING-WORLDS/rocketGuns/bullet.js
+++ FACING-WORLDS/rocketGuns/bullet.js
@@ -16,47 +16,52 @@
     var timer;
     var lethalRadius;
     
-    this.preload = function(entityID) {
-        var properties = Entities.getEntityProperties(entityID, ["owningAvatarID", "description"]);
-        ownerID = properties.owningAvatarID;
-        lethalRadius = parseInt(properties.description, 10);
-        if (ownerID === MyAvatar.sessionUUID) {
-            timer = Script.setTimeout(function() {
-                Entities.deleteEntity(entityID);
-            }, 20000);
-        }
-    }; 
+    var Bullet = function() {};
 
-    this.collisionWithEntity = function (myID, otherID, collision) {
-        print("AT LEAST IT COLLIDE." ); //######################################################### REMOVE ##
-        var i, messageSent;
-        var position = Entities.getEntityProperties(myID, ["position"]).position;
-        if (ownerID === MyAvatar.sessionUUID) {
-            print("EXPLOSION AT: " + JSON.stringify(position)); //######################################################### REMOVE ##
-            print("By: " + ownerID ); //######################################################### REMOVE ##
-            var victimes = AvatarManager.getAvatarsInRange( position, lethalRadius );
-            Entities.editEntity(myID, {"dynamic": false, "dimension": {"x": lethalRadius * 2, "y": lethalRadius * 2, "z": lethalRadius * 2}});
-            Script.setTimeout(function() {
-                Entities.editEntity(myID, {"visible": false, "dimension": {"x": lethalRadius * 2.2, "y": lethalRadius * 2.2, "z": lethalRadius * 2.2}});
-            }, 700);
-            for (i = 0; i < victimes.length; i++) {
-                messageSent = {
-                    "action": "REVIVE",
-                    "avatarID": victimes[i],
-                    "by": ownerID
-                };
-                Messages.sendMessage(channelComm, JSON.stringify(messageSent));
-
-                messageSent = {
-                    "action": "KILL",
-                    "position": position
-                };
-                Messages.sendMessage(channelComm, JSON.stringify(messageSent));
+    Bullet.prototype = {
+        preload: function(entityID) {
+            var properties = Entities.getEntityProperties(entityID, ["owningAvatarID", "description"]);
+            ownerID = properties.owningAvatarID;
+            lethalRadius = parseInt(properties.description, 10);
+            if (ownerID === MyAvatar.sessionUUID) {
+                timer = Script.setTimeout(function() {
+                    Entities.deleteEntity(entityID);
+                }, 20000);
             }
+        },
+        collisionWithEntity: function(myID, otherID, collision) {
+            print("AT LEAST IT COLLIDE." ); //######################################################### REMOVE ##
+            var i, messageSent;
+            var position = Entities.getEntityProperties(myID, ["position"]).position;
+            if (ownerID === MyAvatar.sessionUUID) {
+                print("EXPLOSION AT: " + JSON.stringify(position)); //######################################################### REMOVE ##
+                print("By: " + ownerID ); //######################################################### REMOVE ##
+                var victimes = AvatarManager.getAvatarsInRange( position, lethalRadius );
+                Entities.editEntity(myID, {"dynamic": false, "dimension": {"x": lethalRadius * 2, "y": lethalRadius * 2, "z": lethalRadius * 2}});
+                Script.setTimeout(function() {
+                    Entities.editEntity(myID, {"visible": false, "dimension": {"x": lethalRadius * 2.2, "y": lethalRadius * 2.2, "z": lethalRadius * 2.2}});
+                }, 700);
+                for (i = 0; i < victimes.length; i++) {
+                    messageSent = {
+                        "action": "REVIVE",
+                        "avatarID": victimes[i],
+                        "by": ownerID
+                    };
+                    Messages.sendMessage(channelComm, JSON.stringify(messageSent));
 
-            Script.clearTimeout(timer);
-            Entities.deleteEntity(myID);
+                    messageSent = {
+                        "action": "KILL",
+                        "position": position
+                    };
+                    Messages.sendMessage(channelComm, JSON.stringify(messageSent));
+                }
+
+                Script.clearTimeout(timer);
+                Entities.deleteEntity(myID);
+            }
         }
-
     };
-})
+
+    return new Bullet();
+    
+});
