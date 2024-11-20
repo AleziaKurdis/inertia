cce27c4a4642463275106d9537ca2670ace6c2b6
Alezia Kurdis <60075796+AleziaKurdis@users.noreply.github.com>
2024-11-19 22:21:03 -0500

    Add files via upload

---
+++ ARCADE/pacman/arcadePacMan.js
@@ -0,0 +1,60 @@
+//
+//  arcadePacMan.js
+//
+//  Created by Alezia Kurdis, November 19th, 2024.
+//  Copyright 2024 Overte e.V.
+//
+//  Archade machine for pac-man
+//
+//  Distributed under the Apache License, Version 2.0.
+//  See the accompanying file LICENSE or http://www.apache.org/licenses/LICENSE-2.0.html
+//
+(function() {
+    var jsMainFileName = "arcadePacMan.js"; 
+    var ROOT = Script.resolvePath('').split(jsMainFileName)[0];
+    
+    var channel = "overte.ak.arcadeGame.pacman";
+    var webID = Uuid.NONE;
+    var thisEntityID;
+    
+    this.preload = function(entityID) {
+        thisEntityID = entityID;
+        
+        webID = Entities.addEntity({
+            "type": "Web",
+            "name": "arcade game screen",
+            "parentID": entityID,
+            "dimensions": {"x": 0.6, "y": 0.4, "z": 0.01},
+            "localPosition":  {"x": 0.0, "y": 0.0, "z": 0.0},
+            "sourceUrl": ROOT + "index.html",
+            "dpi": 12,
+            "maxFPS": 60,
+            "ignorePickIntersection": true
+        }, "local");
+    };
+
+    this.unload = function(entityID) {
+        if (webID !== Uuid.NONE) {
+            Entities.deleteEntity(webID);
+            webID = Uuid.NONE;
+        }
+    };
+
+    webID.webEventReceived.connect(function (message) {
+        if (typeof message === "string") {
+            var d = new Date();
+            var n = d.getTime();
+            var instruction = JSON.parse(message);
+            if (instruction.channel === channel) {
+                if (instruction.action === "ACTION_NAME") {
+                    //Call a function to do something here
+                }
+            }
+        }
+    });
+
+/*    //to send
+    webID.emitScriptEvent(stringmessage);
+*/
+
+}());
---
+++ ARCADE/pacman/static/script/overteHtmlControl.js
@@ -0,0 +1,59 @@
+//  overteHtmlControl.js
+//
+//  Created by Alezia Kurdis, November 18th, 2024.
+//  Copyright 2024, Overte e.V.
+//
+//  Plug VR Control on game.
+//
+//  Distributed under the Apache License, Version 2.0.
+//  See the accompanying file LICENSE or http://www.apache.org/licenses/LICENSE-2.0.html
+//
+var channel = "overte.ak.arcadeGame.pacman";
+var gameStatus = "IDLE"; //IDLE | PLAYING | OVER
+
+
+EventBridge.scriptEventReceived.connect(function(message){
+    var messageObj = JSON.parse(message);
+    if (messageObj.channel === channel) {
+        if (messageObj.action === "UP") {
+            if (gameStatus === "PLAYING") {
+                player.control = {orientation:3};
+            }
+        } else if (messageObj.action === "DOWN") {
+            if (gameStatus === "PLAYING") {
+                player.control = {orientation:1};
+            }
+        } else if (messageObj.action === "LEFT") {
+            if (gameStatus === "PLAYING") {
+                player.control = {orientation:2};
+            }
+        } else if (messageObj.action === "RIGHT") {
+            if (gameStatus === "PLAYING") {
+                player.control = {orientation:0};
+            }
+        } else if (messageObj.action === "START-PAUSE") {
+            switch(gameStatus) {
+                case "IDLE":
+                    game.nextStage();
+                    break;
+                case "PLAYING":
+                    stageC.status = stageC.status==2?1:2;
+                    break;
+                case "OVER":
+                    _SCORE = 0;
+                    _LIFE = 5;
+                    game.setStage(1);
+            } 
+        }
+    }
+});
+
+/*
+function uninstall() { //Example of a action called to the application (.js) (you can add the property you need to this, but minimally the channel and the action.
+    var message = {
+        "channel": channel,
+        "action": "SELF_UNINSTALL"
+    };
+    EventBridge.emitWebEvent(JSON.stringify(message));
+}
+*/
