85bfc2d3ab369b660411a1be3644a26fa75e9bce
Alezia Kurdis <60075796+AleziaKurdis@users.noreply.github.com>
2025-05-20 22:37:21 -0400

    Add files via upload

---
+++ BREEMOR/scripts/localTeleporter.js
@@ -0,0 +1,78 @@
+//
+//  localTeleporter.js
+//
+//  Created by Alezia Kurdis, May 20th, 2025.
+//  Copyright 2025, Overte e.V.
+//
+//  Teleportation script for elevator.
+//
+//  Distributed under the Apache License, Version 2.0.
+//  See the accompanying file LICENSE or http://www.apache.org/licenses/LICENSE-2.0.html
+//
+(function(){
+    var ROOT = Script.resolvePath('').split("localTeleporter.js")[0];
+    var arrivalPosition;
+    var SOUND = SoundCache.getSound(ROOT + "../sounds/elevator.mp3");
+    var MAX_CLICKABLE_DISTANCE_M = 2; 
+    var oneTimeOnly = false;
+    
+    this.preload = function(entityID) {
+        arrivalPosition = JSON.parse(Entities.getEntityProperties(entityID, ["description"]).description);
+    }
+
+    
+    // Constructor
+    var _this = null;
+
+    function clickableUI() {
+        _this = this;
+        this.entityID = null;
+    }
+
+    function trigger() {
+        if (oneTimeOnly === false) {
+            MyAvatar.position = arrivalPosition;
+            var injectorOptions = {
+                "position": MyAvatar.position,
+                "volume": 0.5,
+                "loop": false,
+                "localOnly": false
+            };
+            var injector = Audio.playSound(SOUND, injectorOptions);
+            
+            oneTimeOnly = true;
+            Script.setTimeout(function () {
+                oneTimeOnly = false;
+            }, 3000);
+        }
+    }; 
+
+    // Entity methods
+    clickableUI.prototype = {
+        preload: function (id) {
+            _this.entityID = id;
+            HMD.displayModeChanged.connect(this.displayModeChangedCallback);
+        },
+
+        displayModeChangedCallback: function() {
+            if (_this && _this.entityID) {
+                //Nothing
+            }
+        },
+
+        mousePressOnEntity: function (entityID, event) {
+            if (event.isPrimaryButton && 
+                Vec3.distance(MyAvatar.position, Entities.getEntityProperties(_this.entityID, ["position"]).position) <= MAX_CLICKABLE_DISTANCE_M) {
+                    trigger();
+            }
+        },
+
+        unload: function () {
+            HMD.displayModeChanged.disconnect(this.displayModeChangedCallback);
+        }
+    };
+
+    
+    return new clickableUI();
+
+});
