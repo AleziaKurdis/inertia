58241f030f1e2c9d3678e0558d241c22f4f5321a
Alezia Kurdis <60075796+AleziaKurdis@users.noreply.github.com>
2025-08-07 21:54:58 -0400

    Add files via upload

--- ASTROBION/sit_spot.js
+++ ASTROBION/sit_spot.js
@@ -12,6 +12,13 @@
 	let visualID;
 	let selfID;
     let renderWithZones;
+    let thisPosition;
+    let hasBeenViewed;
+    
+    let timerIntervall = 4000; // 4 sec 
+    let processTimer = 0;
+    const DISTANCE_TO_SEE_INDICATOR = 3.0;
+    
     const ROOT = Script.resolvePath('').split("sit_spot.js")[0];
 
 	this.actionEvent = function(actionID, value) {
@@ -20,35 +27,22 @@
 		if (value > 0.5) {
 			MyAvatar.endSit(...previousAvatarXform);
 			Messages.sendLocalMessage(MSG_CHANNEL, JSON.stringify({ seatID: selfID, sitting: false }));
-			Entities.editEntity(visualID, { visible: true });
+			Entities.editEntity(visualID, { "visible": true });
+            hasBeenViewed = true;
 			isSitting = false;
+            Script.setTimeout(function () {
+                Entities.editEntity(visualID, { "visible": false });
+            }, 40000); //40 sec
 		}
 	}
 
 	this.preload = function(_selfID) {
 		selfID = _selfID;
-        renderWithZones = Entities.getEntityProperties(selfID, ["renderWithZones"]).renderWithZones;
+        let properties = Entities.getEntityProperties(selfID, ["position", "renderWithZones"]);
+        renderWithZones = properties.renderWithZones;
+        thisPosition = properties.position;
 		Controller.actionEvent.connect(this.actionEvent);
 
-		/*visualID = Entities.addEntity({
-			"type": "Text",
-			"parentID": selfID,
-			"localRotation": Quat.fromPitchYawRollDegrees(-90, 180, 0),
-			"dimensions": [0.3, 0.3, 0.1],
-			"text": "SIT",
-			"unlit": true,
-			"backgroundAlpha": 0.6,
-			"textEffect": "outline fill",
-			"textEffectColor": [0, 0, 0],
-			"textEffectThickness": 0.4,
-			"alignment": "center",
-			"verticalAlignment": "center",
-			"ignorePickIntersection": true,
-            "renderWithZones": renderWithZones,
-            "grab": {
-                "grabbable": false
-            }
-		}, "local");*/
 		visualID = Entities.addEntity({
 			"type": "Image",
 			"parentID": selfID,
@@ -62,9 +56,38 @@
             "renderWithZones": renderWithZones,
             "grab": {
                 "grabbable": false
-            }
+            },
+            "visible": false
 		}, "local");
+        
+        hasBeenViewed = false;
+        let today = new Date();
+        processTimer = today.getTime();
+        Script.update.connect(myTimer);
 	};
+
+    function myTimer(deltaTime) {
+        let today = new Date();
+        if ((today.getTime() - processTimer) > timerIntervall ) {
+            if (!isSitting) {
+                if (Vec3.distance(MyAvatar.position, thisPosition) < DISTANCE_TO_SEE_INDICATOR) {
+                    if (!hasBeenViewed) {
+                        Entities.editEntity(visualID, { "visible": true });
+                        hasBeenViewed = true;
+
+                        Script.setTimeout(function () {
+                            Entities.editEntity(visualID, { "visible": false });
+                        }, 40000); //40 sec
+                    }
+                } else {
+                    Entities.editEntity(visualID, { "visible": false });
+                    hasBeenViewed = false;
+                }
+            }
+            today = new Date();
+            processTimer = today.getTime();
+        }  
+    }
 
 	this.mousePressOnEntity = function(entityID, event) {
 		if (
@@ -79,7 +102,8 @@
 		previousAvatarXform = [MyAvatar.position, MyAvatar.orientation];
 		MyAvatar.beginSit(Vec3.sum(position, Vec3.multiplyQbyV(rotation, [0, 0.1, 0])), rotation);
 		Messages.sendLocalMessage(MSG_CHANNEL, JSON.stringify({ seatID: selfID, sitting: true }));
-		Entities.editEntity(visualID, { visible: false });
+		Entities.editEntity(visualID, { "visible": false });
+        hasBeenViewed = false;
 		isSitting = true;
 	};
 
@@ -107,6 +131,7 @@
 	};
 
     function unloading() {
+        Script.update.disconnect(myTimer);
         Messages.messageReceived.disconnect(this.messageRecv);
 		Controller.actionEvent.disconnect(this.actionEvent);
 		Entities.deleteEntity(visualID);
