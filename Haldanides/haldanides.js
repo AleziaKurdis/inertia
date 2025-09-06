d8c234bf6c639e137e1e51934138048656c147d7
Alezia Kurdis <60075796+AleziaKurdis@users.noreply.github.com>
2025-09-05 21:11:02 -0400

    Add files via upload

--- Haldanides/haldanides.js
+++ Haldanides/haldanides.js
@@ -138,7 +138,7 @@
         var d19CurrentHour = (GetCurrentCycleValue(86400000, DAY_DURATION)/1000) / 3600;
         
         //const TARGET_HOUR = 22.5;
-        const TARGET_HOUR = 17.35; //DEBUG
+        const TARGET_HOUR = 22.0; //DEBUG
         
         const RANDOM_CATALYZER = 0.2;
         
