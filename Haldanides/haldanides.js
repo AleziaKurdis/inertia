78e46394a61242cca7d8a25b1fe7d473a7b6e64b
Alezia Kurdis <60075796+AleziaKurdis@users.noreply.github.com>
2025-09-09 21:28:15 -0400

    Add files via upload

--- Haldanides/haldanides.js
+++ Haldanides/haldanides.js
@@ -185,7 +185,7 @@
                 
                 const lifeTime = 38;
                 let meteorRotation = Quat.multiply(triggerRotation, Quat.fromVec3Degrees({"x": 0.0, "y": 0.0, "z": (Math.random() * 40) - 20}));
-                let meteorPosition = Vec3.sum(triggerPosition, {"x": (Math.random() * 2000) - 1000, "y": 100 + (Math.random() * 300), "z": -5000});
+                let meteorPosition = Vec3.sum(triggerPosition, {"x": (Math.random() * 1600) - 800, "y": 100 + (Math.random() * 300), "z": -5000});
                 let halID = Entities.addEntity({
                     "renderWithZones": renderWithZones,
                     "position": Vec3.multiplyQbyV(meteorRotation, meteorPosition),
