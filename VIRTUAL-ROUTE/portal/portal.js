04afb46063599039073f9773c53b5f0a6ea959a5
Alezia Kurdis <60075796+AleziaKurdis@users.noreply.github.com>
2025-12-18 23:59:58 -0500

    Add files via upload

--- VIRTUAL-ROUTE/portal/portal.js
+++ VIRTUAL-ROUTE/portal/portal.js
@@ -122,7 +122,7 @@
                 "grab": {
                     "grabbable": false
                 },
-                "angularVelocity": Vec3.multiplyQbyV(portalRotation, {"x": 0.0, "y": 0.0, "z": -0.31415900588035583}),
+                //"angularVelocity": Vec3.multiplyQbyV(portalRotation, {"x": 0.0, "y": 0.0, "z": -0.31415900588035583}),
                 "damping": 0,
                 "angularDamping": 0,
                 "collisionless": true,
