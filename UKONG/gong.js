1d578211c3bd26668bf25867df702d01548aabb3
Alezia Kurdis <60075796+AleziaKurdis@users.noreply.github.com>
2026-04-07 19:43:46 -0400

    Add files via upload

--- UKONG/gong.js
+++ UKONG/gong.js
@@ -27,7 +27,7 @@
 
     function trigger(entityID) {
         let d17CurrentHour = (GetCurrentCycleValue(8640000, D17_DAY_DURATION)/100) / 3600;
-        if ( d17CurrentHour > 20 && d17CurrentHour < 4) {
+        if ( d17CurrentHour > 19 && d17CurrentHour < 5) {
             let messageToSend = {
                 "action": "START_ZAMYRU_RITUAL_OF_DEATH"
             };
