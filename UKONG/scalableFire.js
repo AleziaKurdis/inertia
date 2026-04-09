f36d9cf48bc6ddc731b8b864fe6e8dafeb548ee6
Alezia Kurdis <60075796+AleziaKurdis@users.noreply.github.com>
2026-04-08 20:57:26 -0400

    Add files via upload

---
+++ UKONG/scalableFire.js
@@ -0,0 +1,136 @@
+//
+//  scalableFire.js
+//
+//  Created by Alezia Kurdis, April 8th, 2026.
+//  Copyright 2026, Overte e.V.
+//
+//  scalable fire for the UKONG scenery.
+//
+//  Distributed under the Apache License, Version 2.0.
+//  See the accompanying file LICENSE or http://www.apache.org/licenses/LICENSE-2.0.html
+//
+(function(){
+    var ROOT = Script.resolvePath('').split("scalableFire.js")[0];
+    const LIGHT_INTENSITY = 10.0;
+
+    let timer;
+    let entitiesToDelete = [];
+    
+    this.preload = function(entityID) {
+        const properties = Entities.getEntityProperties(entityID, ["renderWithZones", "dimensions"]);
+        const renderWithZones = properties.renderWithZones;
+        let scale = properties.dimensions.x > properties.dimensions.z ? properties.dimensions.z : properties.dimensions.x;
+
+        let id = Entities.addEntity({
+            "name": "Scalable Fire Light",
+            "type": "Light",
+            "localPosition": { "x": 0.0, "y": 0.0, "z": 0.0},
+            "parentID": entityID,
+            "renderWithZones": renderWithZones,
+            "grab": {
+                "grabbable": false
+            },
+            "dimensions": {
+                "x": 30 * scale,
+                "y": 30 * scale,
+                "z": 30 * scale
+            },
+            "color": {"red": 255, "green": 128, "blue": 0},
+            "intensity": LIGHT_INTENSITY,
+            "falloffRadius": LIGHT_INTENSITY/10,
+            "isSpotlight": false
+        }, "local");
+        entitiesToDelete.push(id);
+        
+        let particleID = Entities.addEntity({
+            "type": "ParticleEffect",
+            "localPosition": {
+                "x": 0.0,
+                "y": 0.0,
+                "z": 0.0
+            },
+            "name": "Scalable Fire Flamme FX",
+            "dimensions": {
+                "x": 0.8235000371932983 * scale,
+                "y": 0.8235000371932983 * scale,
+                "z": 0.8235000371932983 * scale
+            },
+            "parentID": entityID,
+            "renderWithZones": renderWithZones,
+            "grab": {
+                "grabbable": false
+            },
+            "shapeType": "ellipsoid",
+            "color": {
+                "red": 255,
+                "green": 162,
+                "blue": 41
+            },
+            "alpha": 0.15,
+            "textures": ROOT + "/images/flambeau.png",
+            "maxParticles": 8,
+            "lifespan": 0.5,
+            "emitRate": 16,
+            "emitSpeed": 0,
+            "speedSpread": 0,
+            "emitOrientation": {
+                "x": 0,
+                "y": 0,
+                "z": 0,
+                "w": 1
+            },
+            "emitDimensions": {
+                "x": 0.05000000074505806,
+                "y": 0,
+                "z": 0.05000000074505806
+            },
+            "emitRadiusStart": 0,
+            "polarFinish": 3.1415927410125732,
+            "emitAcceleration": {
+                "x": 0,
+                "y": 1.2000000476837158,
+                "z": 0
+            },
+            "accelerationSpread": {
+                "x": 0,
+                "y": 0.20000000298023224,
+                "z": 0
+            },
+            "particleRadius": 0.15000000596046448 * scale,
+            "radiusSpread": 0.05000000074505806 * scale,
+            "radiusStart": 0.15000000596046448 * scale,
+            "radiusFinish": 0.15000000596046448 * scale,
+            "colorStart": {
+                "red": 255,
+                "green": 223,
+                "blue": 64
+            },
+            "colorFinish": {
+                "red": 255,
+                "green": 115,
+                "blue": 0
+            },
+            "emitterShouldTrail": false,
+            "alphaSpread": 0.10000000149011612,
+            "alphaStart": 0.4000000059604645,
+            "alphaFinish": 0,
+            "spinSpread": 0.17000000178813934,
+            "spinStart": null,
+            "spinFinish": null
+        }, "local");
+        entitiesToDelete.push(particleID);
+        
+        timer = Script.setInterval(function () {
+            Entities.editEntity(id, {"intensity": ((LIGHT_INTENSITY/2) + (Math.random() * LIGHT_INTENSITY))});
+        }, 50);
+    };
+
+    this.unload = function(entityID) {
+        Script.clearInterval(timer);
+        for (let i=0; i < entitiesToDelete.length; i++) {
+            Entities.deleteEntity(entitiesToDelete[i]);
+        }
+        entitiesToDelete = [];
+    };
+
+})
