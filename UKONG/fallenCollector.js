ba761f43d606eb47673e69a616968b1eacc62d89
Alezia Kurdis <60075796+AleziaKurdis@users.noreply.github.com>
2026-04-10 21:58:20 -0400

    Add files via upload

--- UKONG/fallenCollector.js
+++ UKONG/fallenCollector.js
@@ -4,7 +4,7 @@
 //  Created by Alezia Kurdis, December 26th, 2025.
 //  Copyright 2025, Overte e.V.
 //
-//  Return fallen avatars for the VALOMBRA scenery.
+//  Return fallen avatars for the Yu'Kong scenery.
 //
 //  Distributed under the Apache License, Version 2.0.
 //  See the accompanying file LICENSE or http://www.apache.org/licenses/LICENSE-2.0.html
@@ -19,6 +19,8 @@
     };
 
     this.enterEntity = function(entityID) {
+        MyAvatar.velocity = {"x": 0.0, "y": 0.0, "z": 0.0};
+        MyAvatar.angularVelocity = {"x": 0.0, "y": 0.0, "z": 0.0};
         MyAvatar.position = returnPosition;
         MyAvatar.orientation = returnRotation;
     }
