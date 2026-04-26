a43bc2406febec55011e174f7bdbaf6e09fd9cb7
Alezia Kurdis <60075796+AleziaKurdis@users.noreply.github.com>
2026-04-25 21:28:56 -0400

    Add files via upload

--- UKONG/soundFxManager.js
+++ UKONG/soundFxManager.js
@@ -13,6 +13,7 @@
     const ROOT = Script.resolvePath('').split("soundFxManager.js")[0];
     let thisEntityID;
     let renderWithZones;
+    let entityPosition;
     
     let updateTimerInterval = 4000; //4sec.
     let processTimer = 0;
@@ -23,66 +24,66 @@
         {
             "name": "WILD_DEER", //swamp
             "soundFile": "sounds/WILD/WILD_DEER.mp3",
-            "rawPosition": {"x":-3250.16,"y":3005.54,"z":-3214.09},
+            "rawPosition": {"x": -3250.16,"y": 3005.54,"z": -3214.09},
             "radius": 200.0, //in meters
             "frequency": 80 //in second
         },
         {
             "name": "WILD_MASTODONTE", //swamp
             "soundFile": "sounds/WILD/WILD_MASTODONTE.mp3",
-            "rawPosition": {"x":-3135.4,"y":3005.6,"z":-3180.37},
+            "rawPosition": {"x": -3135.4,"y": 3005.6,"z": -3180.37},
             "radius": 80.0,
             "frequency": 50
         },
         {
             "name": "WILD_HAWK_NOLOOP", //swamp to wall
             "soundFile": "sounds/WILD/WILD_HAWK_NOLOOP.mp3",
-            "rawPosition": {"x":-2953.49,"y":3012.38,"z":-3222.87},
+            "rawPosition": {"x": -2953.49,"y": 3012.38,"z": -3222.87},
             "radius": 200.0,
             "frequency": 60
         },
         {
             "name": "WILD_MONKEY", //jungle
             "soundFile": "sounds/WILD/WILD_MONKEY.mp3",
-            "rawPosition": {"x":-3212.76,"y":3000.0,"z"-2755.71},
+            "rawPosition": {"x": -3212.76,"y": 3000.0,"z": -2755.71},
             "radius": 250.0,
             "frequency": 30
         },
         {
             "name": "WILD_NEARBY", //swamp
             "soundFile": "sounds/WILD/WILD_NEARBY.mp3",
-            "rawPosition": {"x":-3200.78,"y":3005.54,"z":-3215.21},
+            "rawPosition": {"x": -3200.78,"y": 3005.54,"z": -3215.21},
             "radius": 70.0,
             "frequency": 40
         },
         {
             "name": "WILD_ROARS", //swamp
             "soundFile": "sounds/WILD/WILD_ROARS.mp3",
-            "rawPosition": {"x":-3230.08,"y":3004.88,"z":-3275.51},
+            "rawPosition": {"x": -3230.08,"y": 3004.88,"z": -3275.51},
             "radius": 60.0,
             "frequency": 60
         },
         {
             "name": "WILD_RRRR", //swamp
             "soundFile": "sounds/WILD/WILD_RRRR.mp3",
-            "rawPosition": {"x":-3266.9,"y":3004.92,"z":-3288.05},
+            "rawPosition": {"x": -3266.9,"y": 3004.92,"z": -3288.05},
             "radius": 80.0,
             "frequency": 50
         },
         {
             "name": "WILD_WALKING_NEAR", //swamp
             "soundFile": "sounds/WILD/WILD_WALKING_NEAR.mp3",
-            "rawPosition": {"x":-3318.82,"y":3005.56,"z":-3199.92},
+            "rawPosition": {"x": -3318.82,"y": 3005.56,"z": -3199.92},
             "radius": 30.0,
             "frequency": 30
         }
     ];
     
-    let entitiesToDelete = [];
-    
     this.preload = function(entityID) {
-        thisEntityID = entityID
-        renderWithZones = Entities.getEntityProperties(entityID, ["renderWithZones"]).renderWithZones;
+        thisEntityID = entityID;
+        let properties = Entities.getEntityProperties(entityID, ["position", "renderWithZones"]);
+        entityPosition = properties.position;
+        renderWithZones = properties.renderWithZones;
 
         Script.update.connect(myTimer);
         processTimer = Date.now();
@@ -100,9 +101,11 @@
     function manageSounds() {
         let id, localPosition;
         for (let i = 0; i < punctualEventSounds.length; i++) {
+            //localPosition = Vec3.subtract(punctualEventSounds[i].rawPosition, COORD_REFERENCE);
+            //if (Vec3.distance(MyAvatar.position, Vec3.sum(entityPosition, localPosition)) < punctualEventSounds[i].radius) {}
             if (Math.random() < ((updateTimerInterval/1000)/punctualEventSounds[i].frequency)) {
                 localPosition = Vec3.subtract(punctualEventSounds[i].rawPosition, COORD_REFERENCE);
-                localPosition = Vec3.sum(localPosition, Vec3.multiplyQbyV(Quat.fromVec3Degrees( {"x":0.0 ,"y": Math.randon() * 360,"z": 0.0} ),{"x":0.0 ,"y":0.0 ,"z": punctualEventSounds[i].radius}));
+                localPosition = Vec3.sum(localPosition, Vec3.multiplyQbyV(Quat.fromVec3Degrees( {"x":0.0 ,"y": Math.random() * 360,"z": 0.0} ),{"x":0.0 ,"y":0.0 ,"z": Math.sqrt(Math.random()) * punctualEventSounds[i].radius}));
                 id = Entities.addEntity({
                     "type": "Sound",
                     "parentID": thisEntityID,
