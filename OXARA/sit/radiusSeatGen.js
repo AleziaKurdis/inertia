a00c9b1400fe37edd72df47e0a08ba5c77ce37c5
Alezia Kurdis <60075796+AleziaKurdis@users.noreply.github.com>
2026-07-07 22:12:31 -0400

    Add files via upload

---
+++ OXARA/sit/radiusSeatGen.js
@@ -0,0 +1,83 @@
+//
+//  radiusSeatGen.js
+//
+//  Created by Alezia Kurdis, July 7th, 2026.
+//  Copyright 2026, Overte e.V.
+//
+//  Generate sit spots for NORETH in OXARA Scenery.
+//
+//  Distributed under the Apache License, Version 2.0.
+//  See the accompanying file LICENSE or http://www.apache.org/licenses/LICENSE-2.0.html
+//
+(function() {
+    const ROOT = Script.resolvePath('').split("radiusSeatGen.js")[0];
+    let thisEntity;
+    let renderWithZones;
+    let entPosition;
+    let seatID = Uuid.NONE;
+    let processing = null;
+    
+    const MAX_DIST = 5; //in meters
+    
+    this.preload = function(entityID) { 
+        thisEntity = entityID;
+        let properties = Entities.getEntityProperties(entityID, ["renderWithZones", "position"]);
+        renderWithZones = properties.renderWithZones;
+        entPosition = properties.position;
+
+        processing = Script.setInterval(function () {
+            generateSeat();
+        }, 2000);
+    }
+
+    function generateSeat() {
+        let distance = Vec3.distance(entPosition, MyAvatar.position);
+        
+        let distanceOverrider = {
+            "distanceToSeeIndicator": 2.0
+        };
+        
+        let localRotation = Quat.lookAtSimple( {"x":entPosition.x,"y":0.0,"z":entPosition.z}, MyAvatar.position{"x":MyAvatar.position.x,"y":0.0,"z":MyAvatar.position.z});
+        let localPosition = Vec3.multiplyQbyV( localRotation, {"x":0.0,"y":0.0,"z": 4} );
+        
+        if (distance < MAX_DIST) {
+            if (seatID === Uuid.NONE) {
+                seatID = Entities.addEntity({
+                    "type": "Shape",
+                    "shape": "Cube",
+                    "parentID": thisEntity,
+                    "name": "Seat",
+                    "dimensions": {"x":0.3,"y":0.01,"z":0.3},
+                    "localPosition": localPosition,
+                    "localRotation": localRotation,
+                    "renderWithZones": renderWithZones,
+                    "grab": {
+                        "grabbable": false
+                    },
+                    "visible": true,
+                    "alpha": 0.0,
+                    "script": ROOT + "sit_spot.js",
+                    "userData": JSON.stringify(distanceOverrider),
+                    "lifetime": 864000
+                }, "local");
+            } else {
+                //edit (move)
+                Entities.editEntity(seatID, {
+                    "localPosition": localPosition,
+                    "localRotation": localRotation
+                });
+            }
+        }
+    }
+
+    this.unload = function(entityID) {
+        if (processing) { 
+            Script.clearInterval(processing);
+        }
+        if (seatID !== Uuid.NONE) {
+            Entities.deleteEntity(seatID);
+            seatID = Uuid.NONE;
+        }
+    };
+
+})
---
+++ OXARA/sit/sit_spot.js
@@ -0,0 +1,173 @@
+// SPDX-License-Identifier: CC0-1.0
+// sit_spot.js
+//
+//  Created by ada-tv, July 18th, 2025.
+//  Modified by Alezia Kurdis, August 7th, 2025.
+//  Copyright 2025, ada-tv.
+//
+//  Sit_spot, but that vanish after 40 second and reactivate when we approach to at least 3m.
+//
+//  SPDX-License-Identifier: CC0-1.0
+//
+(function() {try{
+	const MSG_CHANNEL = "net.thingvellir.sit_spot";
+	const EDIT_SETTING = "io.highfidelity.isEditing"; // true if the create app is open
+
+	const actionTranslateY = Controller.findAction("TranslateY");
+	let previousAvatarXform = [Vec3.ZERO, Quat.IDENTITY];
+	let isSitting = false;
+	let disabled = false;
+	let visualID;
+	let selfID;
+    let renderWithZones;
+    let thisPosition;
+    let hasBeenViewed;
+    
+    let timerIntervall = 4000; // 4 sec 
+    let processTimer = 0;
+    let distanceToSeeIndicator = 3.0;
+    
+    /*
+    NOTE: distanceToSeeIndicator can be overriden using userData:
+    {
+        "distanceToSeeIndicator": 2.0
+    }
+    */
+    
+    const ROOT = Script.resolvePath('').split("sit_spot.js")[0];
+
+	function actionEvent (actionID, value) {
+		if (disabled || !isSitting || actionID != actionTranslateY) { return; }
+
+		if (value > 0.5) {
+			MyAvatar.endSit(...previousAvatarXform);
+			Messages.sendLocalMessage(MSG_CHANNEL, JSON.stringify({ seatID: selfID, sitting: false }));
+			Entities.editEntity(visualID, { "visible": true });
+            hasBeenViewed = true;
+			isSitting = false;
+            Script.setTimeout(function () {
+                Entities.editEntity(visualID, { "visible": false });
+            }, 40000); //40 sec
+		}
+	}
+
+	this.preload = function(_selfID) {
+		selfID = _selfID;
+        let properties = Entities.getEntityProperties(selfID, ["position", "renderWithZones", "userData"]);
+        renderWithZones = properties.renderWithZones;
+        thisPosition = properties.position;
+        try {
+			let userData = JSON.parse(properties.userData);
+
+			if (userData.distanceToSeeIndicator !== undefined) { 
+                distanceToSeeIndicator = userData.distanceToSeeIndicator;
+            }
+
+		} catch(e) {
+			console.error(e);
+		}
+        
+		Controller.actionEvent.connect(actionEvent);
+
+		visualID = Entities.addEntity({
+			"type": "Image",
+			"parentID": selfID,
+            "localPosition": {"x": 0.0, "y": 0.0, "z": 0.0},
+			"localRotation": Quat.fromPitchYawRollDegrees(-90, 180, 0),
+			"dimensions": [0.3, 0.3, 0.1],
+			"imageURL": ROOT + "sit.svg",
+			"emissive": true,
+			"alpha": 0.6,
+			"ignorePickIntersection": true,
+            "renderWithZones": renderWithZones,
+            "grab": {
+                "grabbable": false
+            },
+            "visible": false
+		}, "local");
+        
+        hasBeenViewed = false;
+        let today = new Date();
+        processTimer = today.getTime();
+        Script.update.connect(myTimer);
+	};
+
+    function myTimer(deltaTime) {
+        let today = new Date();
+        if ((today.getTime() - processTimer) > timerIntervall ) {
+            if (!isSitting) {
+                if (Vec3.distance(MyAvatar.position, thisPosition) < distanceToSeeIndicator) {
+                    if (!hasBeenViewed) {
+                        Entities.editEntity(visualID, { "visible": true });
+                        hasBeenViewed = true;
+
+                        Script.setTimeout(function () {
+                            Entities.editEntity(visualID, { "visible": false });
+                        }, 40000); //40 sec
+                    }
+                } else {
+                    Entities.editEntity(visualID, { "visible": false });
+                    hasBeenViewed = false;
+                }
+            }
+            today = new Date();
+            processTimer = today.getTime();
+        }  
+    }
+
+	this.mousePressOnEntity = function(entityID, event) {
+		if (
+			disabled ||
+			entityID !== selfID ||
+			isSitting ||
+			!event.isPrimaryButton ||
+			Settings.getValue(EDIT_SETTING)
+		) { return; }
+
+		const {position, rotation} = Entities.getEntityProperties(selfID, ["position", "rotation"]);
+		previousAvatarXform = [MyAvatar.position, MyAvatar.orientation];
+		MyAvatar.beginSit(Vec3.sum(position, Vec3.multiplyQbyV(rotation, [0, 0.1, 0])), rotation);
+		Messages.sendLocalMessage(MSG_CHANNEL, JSON.stringify({ seatID: selfID, sitting: true }));
+		Entities.editEntity(visualID, { "visible": false });
+        hasBeenViewed = false;
+		isSitting = true;
+	};
+
+
+	function messageRecv (channel, message, senderID, localOnly) {
+		if (channel !== MSG_CHANNEL || !localOnly || senderID != MyAvatar.sessionUUID) { return; }
+
+		try {
+			let data = JSON.parse(message);
+
+			if (data.seatID == selfID) { return; }
+
+			if (data.sitting !== undefined) {
+				disabled = !!data.sitting;
+			}
+		} catch(e) {
+			console.error(e);
+		}
+	}
+
+	Messages.messageReceived.connect(messageRecv);
+
+	this.unload = function(_selfID) {
+		unloading();
+	};
+
+    function unloading() {
+        Script.update.disconnect(myTimer);
+        Messages.messageReceived.disconnect(messageRecv);
+		Controller.actionEvent.disconnect(actionEvent);
+		Entities.deleteEntity(visualID);
+		delete visualID;
+		if (isSitting) {
+			MyAvatar.endSit(MyAvatar.position, MyAvatar.orientation);
+		}
+    }
+
+	Window.domainChanged.connect(function() {
+		unloading();
+	});
+} catch(e) { console.error(e); unloading(); }})
