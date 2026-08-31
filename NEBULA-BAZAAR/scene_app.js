89e718ea20fe243c1b178114fc931deccc52e00f
Alezia Kurdis <60075796+AleziaKurdis@users.noreply.github.com>
2026-08-30 21:28:29 -0400

    Add files via upload

---
+++ NEBULA-BAZAAR/scene_app.js
@@ -0,0 +1,182 @@
+//
+//  scene_app.js
+//
+//  Created by Alezia Kurdis, August 30th 2026.
+//  Copyright 2026 Overte e.V.
+//
+//  Scene application for Nebula-Bazaar.
+//
+//  Distributed under the Apache License, Version 2.0.
+//  See the accompanying file LICENSE or http://www.apache.org/licenses/LICENSE-2.0.html
+//
+(function() {
+    var jsMainFileName = "scene_app.js";
+    var ROOT = Script.resolvePath('').split(jsMainFileName)[0];
+    
+    var APP_NAME = "SCENE";
+    var APP_URL = ROOT + "application/scene.html";
+    var APP_ICON_INACTIVE = ROOT + "application/icon_inactive.png";
+    var APP_ICON_ACTIVE = ROOT + "application/icon_active.png";
+    var appStatus = false;
+    var ICON_CAPTION_COLOR = "##ffae00";
+    var button;
+    var isRunning = false;
+    
+    var thisPosition;
+    var UPDATE_TIMER_INTERVAL = 5000;
+    
+    var channel = "nebulaBazaar.application.ak.scene";
+    var timestamp = 0;
+    var INTERCALL_DELAY = 200; //0.3 sec
+    
+    var tablet = Tablet.getTablet("com.highfidelity.interface.tablet.system");
+    
+    let timerStatus = false;
+    let timer;
+    
+    this.preload = function(entityID) {
+        var properties = Entities.getEntityProperties(entityID, ["position"]);
+        thisPosition = properties.position;
+        
+        //Create Light here
+        
+        
+        timer = Script.setInterval(checkDistance, UPDATE_TIMER_INTERVAL);
+        timerStatus = true;
+    }
+
+    this.unload = function(entityID) {
+        cleanup();
+    }
+
+    function checkDistance() {
+        if (Vec3.distance(MyAvatar.position, thisPosition) > 30) {
+            if (isRunning) {
+                tablet.screenChanged.disconnect(onScreenChanged);
+                if (button) {
+                    button.clicked.disconnect(clicked);
+                    tablet.removeButton(button);
+                    button = null;
+                }
+                isRunning = false;
+            }
+        } else {
+            if (!isRunning) {
+                tablet.screenChanged.connect(onScreenChanged);
+
+                button = tablet.addButton({
+                    "text": APP_NAME,
+                    "icon": APP_ICON_INACTIVE,
+                    "activeIcon": APP_ICON_ACTIVE,
+                    "sortOrder": 0,
+                    "captionColor": ICON_CAPTION_COLOR
+                });
+                isRunning = true;
+                
+                button.clicked.connect(clicked);
+            }
+        }
+    }
+
+    function onAppWebEventReceived(message) {
+        if (typeof message === "string") {
+            var d = new Date();
+            var n = d.getTime();
+
+            var instruction;
+            try {
+                instruction = JSON.parse(message);
+            } catch(e) {
+                return;
+            }
+            
+            if (instruction.channel === channel) {
+/*              if (instruction.action === "SET_S/ORT" && (n - timestamp) > INTERCALL_DELAY) {
+                    d = new Date();
+                    timestamp = d.getTime();
+                    Settings.setValue(PASSPORT_SORT_SETTING, instruction.sorting);
+                } else if (instruction.action === "UI_READY") {
+                    
+                    var payload = {
+                        "virtualRoute": virtualRoute,
+                        "list": Settings.getValue(PASSPORT_SETTING, []),
+                        "sort": Settings.getValue(PASSPORT_SORT_SETTING, "OMEGA"),
+                    };
+                    
+                    var messageToSent = {
+                        "channel": channel,
+                        "action": "PASSPORT_DATA",
+                        "data": payload
+                    };
+                    tablet.emitScriptEvent(JSON.stringify(messageToSent));
+                } */
+            }
+        }
+    }
+
+    function clicked(){
+        var colorCaption;
+        if (appStatus === true) {
+            tablet.webEventReceived.disconnect(onAppWebEventReceived);
+            tablet.gotoHomeScreen();
+            colorCaption = ICON_CAPTION_COLOR;
+            appStatus = false;
+        }else{
+            tablet.gotoWebScreen(APP_URL);
+            tablet.webEventReceived.connect(onAppWebEventReceived);
+            colorCaption = "#000000";
+            appStatus = true;
+        }
+
+        button.editProperties({
+            "isActive": appStatus,
+            "captionColor": colorCaption
+        });
+    }
+
+    function onScreenChanged(type, url) {
+        var colorCaption;
+        if (type === "Web" && url.indexOf(APP_URL) !== -1) {
+            colorCaption = "#000000";
+            appStatus = true;
+        } else {
+            colorCaption = ICON_CAPTION_COLOR;
+            appStatus = false;
+        }
+        
+        button.editProperties({
+            isActive: appStatus,
+            captionColor: colorCaption
+        });
+    }
+
+    function onDomainChanged(domain) {
+        cleanup();
+        Window.domainChanged.disconnect(onDomainChanged);
+    }
+
+    Window.domainChanged.connect(onDomainChanged);
+
+    function cleanup() {
+        if (appStatus) {
+            tablet.gotoHomeScreen();
+            tablet.webEventReceived.disconnect(onAppWebEventReceived);
+            appStatus = false;
+        }
+        if (isRunning) {
+            tablet.screenChanged.disconnect(onScreenChanged);
+            if (button) {
+                button.clicked.disconnect(clicked);
+                tablet.removeButton(button);
+                button = null;
+            }
+            isRunning = false;
+        }
+        if (timerStatus) {
+            Script.clearInterval(timer);
+            timerStatus = false;
+        }
+    }
+
+    Script.scriptEnding.connect(cleanup);
+})
